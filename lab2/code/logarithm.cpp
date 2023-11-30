3776da2612cdd7537f893b4df8e47908a49a7493
DikPhase <manunitedea@gmail.com>
2023-11-30 11:52:47 +0100

    Logarithm påbörjad

--- lab2/code/logarithm.cpp
+++ lab2/code/logarithm.cpp
@@ -7,18 +7,41 @@
 #include "polynomial.h"
 
 // ADD implementation of the member functions for class Logarithm
-bool Logarithm::isRoot(double x) const {
-
+Logarithm::~Logarithm() {
+	// Implement if needed
 }
 
-explicit Logarithm::operator std::string() const {
+Logarithm::Logarithm() : constant1{ 0 }, constant2{ 0 }, base{ 2 }, argument(Polynomial({ 0 })) {} // Default constructor
 
+Logarithm::Logarithm(double c1, double c2, int b, const Expression& arg) // Constructor with arguments
+	: constant1(c1), constant2(c2), base(b), argument(arg) {}
+
+void Logarithm::set_base(int newBase) {
+	if (newBase > 1) {
+		base = newBase;
+	}
+	else {
+		std::cerr << "Invalid base";
+	}
+}
+
+bool Logarithm::isRoot(double x) const {
+	return false;
+}
+
+Logarithm::operator std::string() const {
+	// Format constants with two digits after the decimal point
+	std::string formattedConstants = std::format("{:.2f} + {:.2f}", constant1, constant2);
+
+	// Format the Logarithm expression
+	return std::format("{} * Log_{}( {} )", formattedConstants, base, static_cast<std::string>(argument));
 }
 
 double Logarithm::operator()(double x) const {
-
+	double d = x;
+		return d;
 }
 
 Expression* Logarithm::clone() const {
-
+	return new Logarithm(*this);
 }
--- lab2/code/logarithm.h
+++ lab2/code/logarithm.h
@@ -6,18 +6,33 @@
 #pragma once
 
 #include <iostream>
-
+#include <format>
+#include <cmath>
 #include "expression.h"
 
 class Logarithm : public Expression {
     // ADD CODE
 public:
 
+    Logarithm(); // Default constructor
+
+    Logarithm(double constant1, double constant2, int base, const Expression& arguemnt); // Constructor with arguments
+
+    ~Logarithm() override; // Default destructor
+
+    void set_base(int newBase);
+
     bool isRoot(double x) const override;
 
-    explicit operator std::string() const override;
+    operator std::string() const override;
 
     double operator()(double x) const override;
 
     Expression* clone() const override;
+
+private:
+    double constant1;
+    double constant2;
+    int base;
+    const Expression& argument;
 };
